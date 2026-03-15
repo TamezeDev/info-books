e4db620924ae323d6c50994375be88502a2ddda7
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-03-15 15:39:21 +0100

    add catalog-view

---
+++ src/main/java/org/zeki/infobooks/controller/CatalogViewController.java
@@ -0,0 +1,38 @@
+package org.zeki.infobooks.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CatalogViewController implements Initializable {
+
+    @FXML
+    private FlowPane containerBookPane;
+
+    @FXML
+    private Label feedBackLabel;
+
+    @FXML
+    private VBox feedbackBox;
+
+    @FXML
+    private Button goBackBtn;
+
+    @FXML
+    private VBox mainMenuBox;
+
+    @FXML
+    private ImageView titleImg;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
