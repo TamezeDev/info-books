8a7426804a5c8276e7bec8d5631ac13daff9c508
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-03-15 13:25:19 +0100

    add main-menu-view and styles

--- src/main/java/org/zeki/infobooks/controller/MainMenuViewController.java
+++ src/main/java/org/zeki/infobooks/controller/MainMenuViewController.java
@@ -1,11 +1,42 @@
 package org.zeki.infobooks.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuViewController implements Initializable {
+
+    @FXML
+    private ImageView catalogImg;
+
+    @FXML
+    private ImageView exportImg;
+
+    @FXML
+    private ImageView favouritesImg;
+
+    @FXML
+    private Label feedBackLabel;
+
+    @FXML
+    private VBox feedbackBox;
+
+    @FXML
+    private ImageView importImg;
+
+    @FXML
+    private VBox mainMenuBox;
+
+    @FXML
+    private ImageView searchImg;
+
+    @FXML
+    private ImageView titleImg;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
